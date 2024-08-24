d72aca069a0bbfba7eea5bd6f0c8b423a59bbaf4
chetan chowdhari <chetanchaudhari90014@gmail.com>
2024-08-24 21:43:14 +0530

    logger.py

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
-import logging
+# from src.ml_practice_project.logger import logging
+# from src.ml_practice_project.exception import CustomException
+# import sys
 
+# if __name__ == "__main__":
+#     logging.info("the execution has started.")
 
-
-if __name__ == "__main__":
-    logging.info("the execution has started.")
+#     try:
+#         a=1/0    
+#     except Exception as e:
+#         logging.info("Custom Exception")
+#         raise CustomException(e,sys)
--- template.py
+++ template.py
@@ -36,7 +36,7 @@ for file in list_of_files:
         os.makedirs(filedir, exist_ok=True)
         logging.info(f"Creating directing: {filedir} for the file {filename}")
 
-    if(not  os.path.exists(filepath)) or  (os.path.getsize(filepath) == 0):
+    if(not os.path.exists(filepath)) or  (os.path.getsize(filepath) == 0):
         with open(filepath,'w') as f:
             pass
             logging.info(f'creating empty file: {filename}')
