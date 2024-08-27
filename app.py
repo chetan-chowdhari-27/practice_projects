67c116afa2866e6552d7814b802acbc97711bef9
chetan chowdhari <chetanchaudhari90014@gmail.com>
2024-08-27 23:37:30 +0530

    he

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
-
+import sys
 from src.ml_project.logger import logging
-# from src.ml_project.logger import logging 
+from src.ml_project.exception import CustomException 
+
+
 
 if __name__ == "__main__":
     logging.info('the  script is running')
+
+    try:
+        a=1/0
+    except Exception as e:
+        logging.info('Custom Exception')
+        raise CustomException(e,sys)
