9bf9c11a582c05ffeaabab74172dee96be556db4
chetan chowdhari <chetanchaudhari90014@gmail.com>
2024-08-27 22:56:45 +0530

    he

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 
-# # from src.ml_project.logger import logging
+from src.ml_project.logger import logging
 # from src.ml_project.logger import logging 
-# if __name__ == "__main__":
-#     logging.info('the  script is running')
+
+if __name__ == "__main__":
+    logging.info('the  script is running')
