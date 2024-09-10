2c5b9fe13d80482faf78f12e36976138ff2539d0
chetan chowdhari <chetanchaudhari90014@gmail.com>
2024-09-11 00:52:23 +0530

    model trainer in-progress

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from src.ml_project.exception import CustomException
 from src.ml_project.components.data_ingestion import DataIngestionConfig
 from src.ml_project.components.data_ingestion import DataIngestion
 from src.ml_project.components.data_transformation import DataTransformation
+from src.ml_project.components.model_trainer import ModelTrainerConfig,ModelTrainer
 
 if __name__ == "__main__":
     logging.info(' The script is running')
@@ -17,6 +18,11 @@ if __name__ == "__main__":
         #data_transformation_config=DataTransformationConfig()
         data_transformation=DataTransformation()
         train_arr,test_arr,_=data_transformation.initiate_data_transormation(train_data_path,test_data_path)
+
+        # Model trainer 
+        model_trainer = ModelTrainer()
+        print(model_trainer.initiate_model_trainer(train_arr,test_arr))
+
     except Exception as e:
         logging.info('Custom Exception')
         raise CustomException(e,sys)
--- src/ml_project/utlis.py
+++ src/ml_project/utlis.py
@@ -6,6 +6,10 @@ from dotenv import load_dotenv
 import pandas as pd
 import pymysql
 import pickle
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import r2_score
+
+
 
 load_dotenv()
 
@@ -39,6 +43,33 @@ def save_object(file_path, obj):
 
         with open(file_path, "wb") as file_obj:
             pickle.dump(obj, file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+
+def evaluate_models(X_train, y_train, X_test, y_test, models, param):
+    try:
+        report = {}
+
+        for model_name, model in models.items():
+            para = param[model_name]
+
+            gs = GridSearchCV(model, para, cv=3)
+            gs.fit(X_train, y_train)
+
+            model.set_params(**gs.best_params_)
+            model.fit(X_train, y_train)
+
+            y_train_pred = model.predict(X_train)
+            y_test_pred = model.predict(X_test)
+
+            train_model_score = r2_score(y_train, y_train_pred)
+            test_model_score = r2_score(y_test, y_test_pred)
+
+            report[model_name] = test_model_score
+
+        return report
 
     except Exception as e:
         raise CustomException(e, sys)
