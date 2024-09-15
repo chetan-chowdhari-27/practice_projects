bc60c2a1abdb1f4a0752f3f25d48a4647d920e4f
chetan chowdhari <chetanchaudhari90014@gmail.com>
2024-09-16 00:51:27 +0530

    model trainre:

--- src/ml_project/components/model_trainer.py
+++ src/ml_project/components/model_trainer.py
@@ -1,7 +1,10 @@
 import os 
 import sys
+import numpy as np
 from dataclasses import dataclass
-
+from urllib.parse import urlparse
+import mlflow
+import mlflow.sklearn
 from catboost import CatBoostRegressor
 from sklearn.ensemble import (
     AdaBoostRegressor,
@@ -14,6 +17,7 @@ from sklearn.metrics import r2_score
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn.tree import DecisionTreeRegressor
 from xgboost import XGBRegressor
+from sklearn.metrics import mean_squared_error,mean_absolute_error
 
 from src.ml_project.exception import CustomException
 from src.ml_project.logger import logging
@@ -28,6 +32,12 @@ class ModelTrainerConfig:
 class ModelTrainer:
     def __init__(self):
         self.model_trainer_config = ModelTrainerConfig()
+
+    def eval_metrics(self,actual, pred):
+        rmse = np.sqrt(mean_squared_error(actual, pred))
+        mae = mean_absolute_error(actual, pred)
+        r2 = r2_score(actual, pred)
+        return rmse, mae, r2
     
     def initiate_model_trainer(self, train_array, test_array):
         try:
@@ -96,6 +106,35 @@ class ModelTrainer:
 
             print("This is the best model:")
             print(best_model_name)
+
+            model_name = list(params.keys())
+
+            actual_model = ""
+            for model in model_name:
+                if best_model_name == model:
+                    actual_model = actual_model + model
+            
+            best_params = params[actual_model]
+
+            mlflow.set_registry_uri("https://dagshub.com/chetanchaudhari90014/practice_projects.mlflow")
+            tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
+
+            # MlFLow
+            with mlflow.start_run():
+               predicted_qualities = best_model.predict(X_test)
+               (rmse, mae, r2) = self.eval_metrics(y_test, predicted_qualities)
+               
+               mlflow.log_params(best_params)
+               mlflow.log_metric("rmse",rmse)
+               mlflow.log_metric("r2",r2)
+               mlflow.log_metric("mae",mae)
+
+               # model registry does not work with file store 
+               if tracking_url_type_store != "file":
+                   
+                   mlflow.sklearn.log_model(best_model, "model",registred_model_name = actual_model)
+               else:
+                   mlflow.sklearn.log_model(best_model, "model")
 
             if best_model_score < 0.6:
                 raise CustomException("No best model found")
--- src/ml_project/utlis.py
+++ src/ml_project/utlis.py
@@ -1,39 +1,44 @@
 import os
 import sys
-from src.ml_project.logger import logging
 from src.ml_project.exception import CustomException
-from dotenv import load_dotenv
+from src.ml_project.logger import logging
 import pandas as pd
-import pymysql
-import pickle
+from dotenv import load_dotenv
 from sklearn.model_selection import GridSearchCV
 from sklearn.metrics import r2_score
+import pymysql
 
-
+import pickle
+import numpy as np
 
 load_dotenv()
 
-host = os.getenv("host")
-user = os.getenv("user")
-passwords = os.getenv("passwords")
-db = os.getenv("db")
+host=os.getenv("host")
+user=os.getenv("user")
+password=os.getenv("password")
+db=os.getenv('db')
+
+
 
 def read_sql_data():
-    logging.info("Reading from SQL Database Started")
+    logging.info("Reading SQL database started")
     try:
-        mydb= pymysql.connect(
+        mydb=pymysql.connect(
             host=host,
             user=user,
-            password=passwords,
+            password=password,
             db=db
         )
-        logging.info("Connection Established")
-        df = pd.read_sql_query('SELECT * FROM students;',mydb)
+        logging.info("Connection Established",mydb)
+        df=pd.read_sql_query('Select * from students',mydb)
         print(df.head())
 
         return df
-    except Exception as x:
-        raise CustomException(x,sys)
+
+
+
+    except Exception as ex:
+        raise CustomException(ex)
     
 def save_object(file_path, obj):
     try:
@@ -46,28 +51,32 @@ def save_object(file_path, obj):
 
     except Exception as e:
         raise CustomException(e, sys)
-    
 
-def evaluate_models(X_train, y_train, X_test, y_test, models, param):
+def evaluate_models(X_train, y_train,X_test,y_test,models,param):
     try:
         report = {}
 
-        for model_name, model in models.items():
-            para = param[model_name]
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+            para=param[list(models.keys())[i]]
 
-            gs = GridSearchCV(model, para, cv=3)
-            gs.fit(X_train, y_train)
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
 
             model.set_params(**gs.best_params_)
-            model.fit(X_train, y_train)
+            model.fit(X_train,y_train)
+
+            #model.fit(X_train, y_train)  # Train model
 
             y_train_pred = model.predict(X_train)
+
             y_test_pred = model.predict(X_test)
 
             train_model_score = r2_score(y_train, y_train_pred)
+
             test_model_score = r2_score(y_test, y_test_pred)
 
-            report[model_name] = test_model_score
+            report[list(models.keys())[i]] = test_model_score
 
         return report
 
